af6cb789e24898ef989c012380a450acef67ee85
Shubin Ilya <pixelwake@gmail.com>
2015-06-01 04:55:31 +0400

    v2: margin, tooltip with quick info

--- app/event.js
+++ app/event.js
@@ -1,33 +1,27 @@
 // Hover inspect extension for Chrome
 // https://github.com/NV0/hover-inspect
 
-(function(){
+(function() {
 
 	var tabs = {};
 
-	function toggle(tab){
-		if (!tabs[tab.id]) {
-			tabs[tab.id] = Object.create(inspect);
-			tabs[tab.id].activate(tab.id);
-		} else {
-			tabs[tab.id].deactivate();
-			for (var tabId in tabs){
-				if (tabId == tab.id) delete tabs[tabId];
-			}
-		}
-	}
 
 	var inspect = {
 		activate: function(id) {
 			this.id = id;
 
-			chrome.tabs.executeScript(this.id, { file: 'prism.js' });	
-			chrome.tabs.insertCSS(this.id, { file: 'hoverinspect.css' });
-			chrome.tabs.executeScript(this.id, { file: 'hoverinspect.js' }, function() {
-				chrome.tabs.sendMessage(this.id, { action: 'activate' });
+			chrome.tabs.executeScript(this.id, {
+				file: 'prism.js'
+			});
+			chrome.tabs.executeScript(this.id, {
+				file: 'hoverinspect.js'
+			}, function() {
+				chrome.tabs.sendMessage(this.id, {
+					action: 'activate'
+				});
 			}.bind(this));
 
-			chrome.browserAction.setIcon({ 
+			chrome.browserAction.setIcon({
 				tabId: this.id,
 				path: {
 					19: "icon_active.png"
@@ -35,10 +29,12 @@
 			});
 		},
 
-		deactivate: function(id) {
-			chrome.tabs.sendMessage(this.id, { action: 'deactivate' });
+		deactivate: function() {
+			chrome.tabs.sendMessage(this.id, {
+				action: 'deactivate'
+			});
 
-			chrome.browserAction.setIcon({ 
+			chrome.browserAction.setIcon({
 				tabId: this.id,
 				path: {
 					19: "icon.png"
@@ -46,9 +42,20 @@
 			});
 		}
 
+	};
+
+	function toggle(tab) {
+		if (!tabs[tab.id]) {
+			tabs[tab.id] = Object.create(inspect);
+			tabs[tab.id].activate(tab.id);
+		} else {
+			tabs[tab.id].deactivate();
+			for (var tabId in tabs) {
+				if (tabId == tab.id) delete tabs[tabId];
+			}
+		}
 	}
 
 	chrome.browserAction.onClicked.addListener(toggle);
 
 })();
-
